2c4d7d387ae09fbdad85b852cc0eb1c6a5abaf38
Israel Calderon <icalderond@outlook.com>
2019-04-07 11:56:57 -0500

    Refactorizando para responsabilidades

---
+++ App/EscuelaEngine.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using CoreEscuela.Entidades;
+
+namespace CoreEscuela
+{
+    public class EscuelaEngine
+    {
+        #region Constructor
+        public EscuelaEngine()
+        {
+            Inicializar();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Escuela Escuela { get; set; }
+
+
+        #endregion
+
+        #region Methods
+
+        public void Inicializar()
+        {
+            Escuela = new Escuela("Platzi Academy", 2019)
+            {
+                Pais = "México",
+                Ciudad = "Guadalajara",
+                TipoEscuela = TiposEscuela.Primaria
+            };
+
+            Escuela.Cursos = new List<Curso>{
+                 new Curso() {Nombre = "101",Jornada=TiposJornada.Mañana},
+                 new Curso() {Nombre = "201",Jornada=TiposJornada.Mañana},
+                 new Curso() {Nombre = "301",Jornada=TiposJornada.Mañana},
+                 new Curso() {Nombre = "401",Jornada=TiposJornada.Tarde},
+                 new Curso() {Nombre = "501",Jornada=TiposJornada.Tarde}
+            };
+        }
+
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using CoreEscuela.Entidades;
 using static System.Console;
 using System.Collections.Generic;
+using CoreEscuela;
 
 namespace Etapa1
 {
@@ -9,50 +10,9 @@ namespace Etapa1
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Academy", 2019)
-            {
-                Pais = "México",
-                Ciudad = "Guadalajara"
-            };
-
-            escuela.Cursos = new List<Curso>{
-                 new Curso() {Nombre = "101"},
-                new Curso(){Nombre = "201"},
-                new Curso(){Nombre = "301"}
-            };
-
-            var otraColeccion = new List<Curso>
-            {
-                new Curso {  Nombre = "102",  Jornada = TiposJornada.Mañana    },
-                new Curso  { Nombre = "202", Jornada = TiposJornada.Tarde },
-                new Curso  { Nombre = "501", Jornada = TiposJornada.Tarde },
-                new Curso  { Nombre = "501", Jornada = TiposJornada.Mañana }
-            };
-
-            escuela.Cursos.AddRange(otraColeccion);
-            ImprimirCursosEscuela(escuela);
-
-            // escuela.Cursos.RemoveAll(delegate (Curso cur)
-            // {
-            //     return cur.Nombre.Contains("301");
-            // });
-
-            // escuela.Cursos.RemoveAll((Curso cur) => cur.Nombre.Contains("301"));
-
-            // escuela.Cursos.RemoveAll((cur) => cur.Nombre.Contains("301"));
-
-            escuela.Cursos.RemoveAll(cur => cur.Nombre.Contains("301"));
-            escuela.Cursos.RemoveAll(cur => cur.Nombre.Contains("501") && cur.Jornada==TiposJornada.Tarde);
-
-            WriteLine("========================");
-            ImprimirCursosEscuela(escuela);
+            var engine=new EscuelaEngine();
+            ImprimirCursosEscuela(engine.Escuela);
         }
-
-        private static bool Predicado(Curso cursoObj)
-        {
-            return cursoObj.Nombre.Contains("301");
-        }
-
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
             WriteLine("======================");
