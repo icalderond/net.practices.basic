b74b4b81c7f9198af74223c3c9d85dc18c97f1ca
Israel Calderon <icalderond@outlook.com>
2019-04-07 11:44:00 -0500

    Expresiones lambda y delegados

--- Program.cs
+++ Program.cs
@@ -23,38 +23,39 @@ namespace Etapa1
 
             var otraColeccion = new List<Curso>
             {
-                new Curso
-                {
-                    Nombre = "102",
-                    Jornada = TiposJornada.Mañana
-                },
-                new Curso
-                {
-                    Nombre = "202",
-                    Jornada = TiposJornada.Tarde
-                }
+                new Curso {  Nombre = "102",  Jornada = TiposJornada.Mañana    },
+                new Curso  { Nombre = "202", Jornada = TiposJornada.Tarde },
+                new Curso  { Nombre = "501", Jornada = TiposJornada.Tarde },
+                new Curso  { Nombre = "501", Jornada = TiposJornada.Mañana }
             };
 
-            // Curso temporal = new Curso { Nombre = "101-Vacacional", Jornada = TiposJornada.Noche };
-
             escuela.Cursos.AddRange(otraColeccion);
-            // escuela.Cursos.Add(temporal);
             ImprimirCursosEscuela(escuela);
 
-            // WriteLine($"Curso.Hash {temporal.GetHashCode()}");
-            Predicate < Curso > miAlgoritmo = Predicado;
-            escuela.Cursos.RemoveAll(Predicado);
+            // escuela.Cursos.RemoveAll(delegate (Curso cur)
+            // {
+            //     return cur.Nombre.Contains("301");
+            // });
 
-            // escuela.Cursos.Remove(temporal);
+            // escuela.Cursos.RemoveAll((Curso cur) => cur.Nombre.Contains("301"));
+
+            // escuela.Cursos.RemoveAll((cur) => cur.Nombre.Contains("301"));
+
+            escuela.Cursos.RemoveAll(cur => cur.Nombre.Contains("301"));
+            escuela.Cursos.RemoveAll(cur => cur.Nombre.Contains("501") && cur.Jornada==TiposJornada.Tarde);
+
             WriteLine("========================");
             ImprimirCursosEscuela(escuela);
         }
 
         private static bool Predicado(Curso cursoObj)
         {
-            return cursoObj.Nombre.Contains("303");
+            return cursoObj.Nombre.Contains("301");
         }
 
+        ///<SUMARY>
+        ///
+        ///
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
             WriteLine("======================");
