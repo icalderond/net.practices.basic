171bc86773b10ef02b93e7280c7990c4e3ad3be2
Israel Calderon <icalderond@outlook.com>
2019-04-07 09:09:29 -0500

    Estapa 1 finalizada

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela=new Escuela("Platzi Academy",2019){
+                Pais="México",
+                Ciudad="Guadalajara"
+                };
+            
+            escuela.TipoEscuela=TiposEscuela.Primaria;
+           Console.WriteLine(escuela);     
+        }
+    }
+}
